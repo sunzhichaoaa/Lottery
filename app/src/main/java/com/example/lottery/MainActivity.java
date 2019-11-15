d98d811a27b13228d312a65778c7ea1766d14ba1
sunzhichao <sunzhichao@hangsheng.com.cn>
2019-11-15 14:25:07 +0800

    修复已知bug，基本功能实现。

--- app/src/main/java/com/example/lottery/MainActivity.java
+++ app/src/main/java/com/example/lottery/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.lottery;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-
 import android.os.Bundle;
-import android.os.Looper;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
     private TextView ssq_issue;
     public static final String lottery_url = "https://api.6vzz.com/api/cpcx.php?lottery_id=dlt";
     public static final  String ssq_url = "https://api.6vzz.com/api/cpcx.php?lottery_id=ssq";
+    public static final int lottery_id = 0x123;
+    public static final int ssq_id = 0x234;
+    private Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +36,25 @@ public class MainActivity extends AppCompatActivity {
         initView();
         lotterySendRequestWithOkHttp();
         ssqSendRequestWithOkHttp();
+        handler = new Handler(){
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+                switch (msg.what){
+                    case lottery_id:
+                        lottery_result.setText(msg.getData().getString("lottery_result"));
+                        lottery_date.setText(msg.getData().getString("lottery_date"));
+                        lottery_issue.setText(msg.getData().getString("lottery_issue"));
+                        break;
+                    case ssq_id:
+                        ssq_result.setText(msg.getData().getString("ssq_result"));
+                        ssq_date.setText(msg.getData().getString("ssq_date"));
+                        ssq_issue.setText(msg.getData().getString("ssq_issue"));
+                        break;
+                }
+            }
+        };
+
+
 
     }
     private void initView(){
@@ -70,8 +93,10 @@ public class MainActivity extends AppCompatActivity {
                             .url(ssq_url)
                             .build();
                     Response response = client.newCall(request).execute();
-                    String responseData = response.body().string();
-                    ssqParseJsonWithGson(responseData);
+
+                        String responseData = response.body().string();
+                        ssqParseJsonWithGson(responseData);
+
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -86,15 +111,20 @@ public class MainActivity extends AppCompatActivity {
             new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    lottery_result.setText(jsonRootBean.getData().getResult());
-                    lottery_date.setText(jsonRootBean.getData().getResulttime());
-                    lottery_issue.setText(jsonRootBean.getData().getPeriodicalnum());
+                    Message message = new Message();
+                    message.what = lottery_id;
+                    Bundle bundle = new Bundle();
+                    Log.i("好好学习", jsonRootBean.getData().getResult());
+                    bundle.putString("lottery_result",jsonRootBean.getData().getResult());
+                    bundle.putString("lottery_date",jsonRootBean.getData().getResulttime());
+                    bundle.putString("lottery_issue",jsonRootBean.getData().getPeriodicalnum());
+                    message.setData(bundle);
+                    handler.sendMessage(message);
+
                 }
             }).start();
         }else {
-            Looper.prepare();
             Toast.makeText(this,"获取参数错误，显示结果不准确",Toast.LENGTH_LONG).show();
-            Looper.loop();
         }
     }
     private void ssqParseJsonWithGson(String response) {
@@ -105,15 +135,20 @@ public class MainActivity extends AppCompatActivity {
             new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    ssq_result.setText(jsonRootBean.getData().getResult());
-                    ssq_date.setText(jsonRootBean.getData().getResulttime());
-                    ssq_issue.setText(jsonRootBean.getData().getPeriodicalnum());
+                    Message message = new Message();
+                    message.what = ssq_id;
+                    Bundle bundle = new Bundle();
+                    bundle.putString("ssq_result",jsonRootBean.getData().getResult());
+                    bundle.putString("ssq_date",jsonRootBean.getData().getResulttime());
+                    bundle.putString("ssq_issue",jsonRootBean.getData().getPeriodicalnum());
+                    message.setData(bundle);
+                    handler.sendMessage(message);
                 }
             }).start();
         }else {
-            Looper.prepare();
+
             Toast.makeText(this,"获取参数错误，显示结果不准确",Toast.LENGTH_LONG).show();
-            Looper.loop();
+
         }
     }
 }
